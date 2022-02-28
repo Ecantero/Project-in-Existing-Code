14ba3ce0d86fa37bb3183a5028099d99bd93dc7c
Ecantero <38026174+Ecantero@users.noreply.github.com>
2022-02-28 10:37:07 -0700

    added endpoints but are not flushed out yet, created home page to check if callback works

---
+++ back-end/db.js
@@ -0,0 +1,14 @@
+console.disableYellowBox = true;
+firebaseConfig = {
+  apiKey: "AIzaSyCK8UYbVGTsBf6y55f6iitsQNlKVlFc3h0",
+  authDomain: "projects-in-existing-code.firebaseapp.com",
+  projectId: "projects-in-existing-code",
+  storageBucket: "projects-in-existing-code.appspot.com",
+  messagingSenderId: "407952246807",
+  appId: "1:407952246807:web:bb3e486e1349053558f452",
+  measurementId: "G-KCE3GKLX2",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
--- index.js
+++ index.js
@@ -1,6 +1,39 @@
-const express = require('express');
+const bodyParser = require("body-parser");
+const expressSession = require("express-session");
+const express = require("express");
 const app = express();
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
+  );
+  next();
+});
+
+var urlencodedParser = bodyParser.urlencoded({
+  extended: true,
+});
+
+app.use(
+  expressSession({
+    secret: "It'sFreeRealEstate",
+    saveUninitialized: false,
+    resave: false,
+  })
+);
+
+app.get("/home", (req, res) => {
+  console.log("the call has been made");
+  res.send({ name: "Ernesto" });
+});
+app.post("/signup");
+app.get("/login");
+app.post("/updateScore");
+
 const port = 8080;
 app.listen(port, () => {
-    console.log(`Server running on port: ${port}`);
- });
+  console.log(`Server running on port: ${port}`);
+});
