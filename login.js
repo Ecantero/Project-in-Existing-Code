6047cba4a28079ac164e2f72dcc430871a3a643d
Ecantero <38026174+Ecantero@users.noreply.github.com>
2022-03-07 09:32:56 -0700

    added instructons page

---
+++ login.js
@@ -0,0 +1,7 @@
+
+const handleSubmit = () => {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  console.log("you press the login button");
+  console.log(email, password);
+};
